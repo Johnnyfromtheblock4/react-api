2f8e5bbeb2941f79cf9e9de8c4967d4365a0f9d1
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-09-04 14:51:12 +0200

    [UPD] importo axios, useeffect e url api

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,10 @@
-function App() {}
+import { useEffect } from "react";
+import axios from "axios";
+
+const actorsUrl = "https://lanciweb.github.io/demo/api/actresses/";
+
+function App() {
+  return <></>;
+}
 
 export default App;
