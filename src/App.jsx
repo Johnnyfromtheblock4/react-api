bbf49c9a34e5e24abb417c17ed0d37fea14c0076
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-09-04 14:58:13 +0200

    [UPD] useState e card

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 
 const actressUrl = "https://lanciweb.github.io/demo/api/actresses/";
 
-useEffect(() => {
-  axios.get(actressUrl).then((resp) => {
-    console.log(resp.data);
-  });
-}, []);
-
 function App() {
-  return <></>;
+  const [actress, setActress] = useState([]);
+
+  // chiamata AJAX
+  useEffect(() => {
+    axios.get(actressUrl).then((resp) => {
+      setActress(resp.data)
+    });
+  }, []);
+
+  return (
+    <>
+      <div className="containe">
+        <div className="row">
+          <div className="col-12">
+            <h1>Lista Attrici</h1>
+          </div>
+          <div className="col-12-col-md-4">
+            Nome Anno Nascita Nazionalità Biografia Immagine Riconoscimenti
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
